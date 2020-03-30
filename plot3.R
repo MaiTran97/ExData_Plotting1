cb9137df84deee73409f78f06628e9cb5abdcf96
Tran Mai <60600661+MaiTran97@users.noreply.github.com>
2020-03-30 19:16:07 +0700

    Create plot3.R

---
+++ plot3.R
@@ -0,0 +1,15 @@
+consump <- read.table("C:/Users/trant/Downloads/STUDY/COURSERA/Exploring data/household_power_consumption.txt", header=TRUE, sep=";", stringsAsFactors=FALSE, dec=".")
+subconsump <- consump[consump$Date %in% c("1/2/2007","2/2/2007") ,]
+date <- strptime(paste0(subconsump$Date, subconsump$Time, sep=" "), "%d/%m/%Y %H:%M:%S")
+globalActivePower <- as.numeric(subconsump$Global_active_power)
+metering1 <- as.numeric(subconsump$Sub_metering_1)
+metering2 <- as.numeric(subconsump$Sub_metering_2)
+metering3 <- as.numeric(subconsump$Sub_metering_3)
+##plot
+png("plot3.png", width = 480, height = 480)
+plot(date, metering1, type = "l", xlab="", ylab = "Energy sub metering")
+lines(date, metering2, type = "l", col = "red")
+lines(date, metering3, type = "l", col = "blue")
+legend("topright", c("Sub_metering_1", "Sub_metering_2", "Sub_metering_3"), lty=1, lwd=2, col=c("black", "red", "blue"))
+dev.off()
+savehistory("C:/Users/trant/Downloads/STUDY/COURSERA/Exploring data/ExData_Plotting1/plot3.R")
